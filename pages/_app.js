a752578bb666afb23be70b0be9f3bcc2921210bf
Meyers <augustmeyers14@gmail.com>
2024-09-08 00:22:51 -0400

    Basic site info

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 export default function App({
   Component,
@@ -6,8 +7,12 @@ export default function App({
 }) {
   return (
     <div>
+      <Head>
+        <title>August Meyers</title>
+        <link rel="icon" type="image/x-icon" href="/images/favicon.ico" />
+      </Head>
       <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
 
         <Component {...pageProps} />
     </div>
