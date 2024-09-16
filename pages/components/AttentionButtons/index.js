ccca85e40f9fb531675d10d4253a85fa6f9500ac
Meyers <augustmeyers14@gmail.com>
2024-09-16 15:45:42 -0400

    fix/background image link

--- pages/components/AttentionButtons/index.js
+++ pages/components/AttentionButtons/index.js
@@ -6,7 +6,7 @@ export default function AttentionButtons() {
   return (
       <div className={styles.wrapper}>
         <AttentionButton text={"Me"} loc="#down" />
-        <AttentionButton text={"Projects"} loc="/projects" background={"/images/projects/code.png"} />
+        <AttentionButton text={"Projects"} loc="/projects" background={"/images/projects/background.webp"} />
         <AttentionButton text={"Photo"} loc="/photography" background={"/images/photography/background.jpg"} />
       </div>
   );
