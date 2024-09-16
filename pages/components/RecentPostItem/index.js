0d57e414be31a18ea52ab19cf7e308ad14fa12ee
Meyers <augustmeyers14@gmail.com>
2024-09-16 15:59:55 -0400

    Additional content to fill

--- pages/components/RecentPostItem/index.js
+++ pages/components/RecentPostItem/index.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import ColorBox from "../ColorBox";
+import SpacerDots from "../SpacerDots";
 
 export default function PostItem({ dir, id, title, date, exerpt, image }) {
   if (dir == null) {
@@ -9,22 +10,25 @@ export default function PostItem({ dir, id, title, date, exerpt, image }) {
   }
 
   return (
-    <ColorBox>
-      <h1>
-        <Link href={`${dir}/${id}`}>{title}</Link>
-      </h1>
-      <p>{date}</p>
-      <Link href={`${dir}/${id}`}>
-        <Image
-          src={image}
-          alt="Cover image"
-          width={500}
-          height={500}
-          priority={true}
-          style={{ width: "100%", height: "10rem", objectFit: "cover" }}
-        />
-      </Link>
-      <p>{exerpt}</p>
-    </ColorBox>
+    <div style={{ display: "contents" }}>
+      <ColorBox>
+        <h1>
+          <Link href={`${dir}/${id}`}>{title}</Link>
+        </h1>
+        <p>{date}</p>
+        <Link href={`${dir}/${id}`}>
+          <Image
+            src={image}
+            alt="Cover image"
+            width={500}
+            height={500}
+            priority={true}
+            style={{ width: "100%", height: "30vh", objectFit: "cover" }}
+          />
+        </Link>
+        <p>{exerpt}</p>
+      </ColorBox>
+      <SpacerDots />
+    </div>
   );
 }
--- pages/projects.js
+++ pages/projects.js
@@ -5,21 +5,32 @@ import Footer from "./components/Footer";
 import RecentPosts from "./components/RecentPosts";
 import TextWrap from "./components/TextWrap";
 import SpacerDots from "./components/SpacerDots";
+import Link from "next/link";
 
-const dir = "projects"
+const dir = "projects";
 export default function Projects({ allPostsData }) {
   return (
     <div>
       <Header />
       <div>
         <TitlePage image={"/images/projects/background.webp"} header={"Projects"}>
-          <p>Things I&apos;ve tackled in computer science and information technology that aren&apos;t as representable as a &quot;project&quot;</p>
+          <p>
+            Things I&apos;ve tackled in computer science and information technology that aren&apos;t as representable as
+            a &quot;project&quot;
+          </p>
         </TitlePage>
         <TextWrap>
-          <h1 style={{textAlign: "center"}}>Recent</h1>
+          <h1 style={{ textAlign: "center" }}>Recent</h1>
           <SpacerDots />
         </TextWrap>
         <RecentPosts dir={dir} allPostsData={allPostsData} />
+        <TextWrap>
+          <p style={{ textAlign: "center" }}>
+            If you are interested in more of what I have worked on, check out my{" "}
+            <Link href="https://github.com/meyersa">github</Link>. There are a lot of other things I haven't taken the
+            time to write about on there
+          </p>
+        </TextWrap>
       </div>
       <Footer />
     </div>
