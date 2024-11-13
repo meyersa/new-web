a7fd7ad6bde83f17e421d0d084697fd94b3829fe
Meyers <augustmeyers14@gmail.com>
2024-11-12 20:57:14 -0500

    Recent post page update

--- pages/components/RecentPostItem/index.js
+++ pages/components/RecentPostItem/index.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import ColorBox from "../ColorBox";
 import SpacerDots from "../SpacerDots";
+import styles from "./recentpostitem.module.css";
 
 export default function PostItem({ dir, id, title, date, exerpt, image }) {
   if (dir == null) {
@@ -10,24 +11,30 @@ export default function PostItem({ dir, id, title, date, exerpt, image }) {
   }
 
   return (
-    <div style={{ display: "contents" }}>
-      <ColorBox>
-        <h1>
-          <Link href={`${dir}/${id}`}>{title}</Link>
-        </h1>
-        <p>{date}</p>
-        <Link href={`${dir}/${id}`}>
-          <Image
-            src={image}
-            alt="Cover image"
-            width={500}
-            height={500}
-            priority={true}
-            style={{ width: "100%", height: "30vh", objectFit: "cover" }}
-          />
-        </Link>
-        <p>{exerpt}</p>
-      </ColorBox>
+    <div className={styles.contents}>
+      <div className={styles.wrapper}>
+        <div className={styles.text}>
+          <h1>
+            <Link href={`${dir}/${id}`}>{title}</Link>
+          </h1>
+          <p>{date}</p>
+          <p>{exerpt}</p>
+        </div>
+        <div className={styles.target}>
+          <ColorBox>
+            <Link href={`${dir}/${id}`}>
+              <Image
+                src={image}
+                alt="Cover image"
+                width={500}
+                height={500}
+                priority={true}
+                style={{ width: "100%", height: "30vh", objectFit: "cover" }}
+              />
+            </Link>
+          </ColorBox>
+        </div>
+      </div>
       <SpacerDots />
     </div>
   );
