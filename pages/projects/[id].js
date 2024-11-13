fd7ed77274a984247342f34bfddf883ce2d89dff
Meyers <augustmeyers14@gmail.com>
2024-11-13 11:36:47 -0500

    Add navigational links

--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Header from "../components/Header";
 import TitlePage from "../components/TitlePage";
 import Footer from "../components/Footer";
@@ -6,6 +7,45 @@ import AuthorDate from "../components/AuthorDate";
 import TextWrap from "../components/TextWrap";
 
 export default function Writeup({ postData }) {
+  const [contentHtml, setContentHtml] = useState(postData.contentHtml);
+  const [headings, setHeadings] = useState([]);
+
+  useEffect(() => {
+    /*
+     * Display a table of contents at the top of posts
+     * - Parse HTML from postData to find h1s, and then
+     *   go back and add IDs to them
+     * - Those IDs are then saved and passed to the nav
+     *   to be used for scrolling to them
+     */
+
+    // Temp container for parsing HTML
+    const tempDiv = document.createElement("div");
+    tempDiv.innerHTML = postData.contentHtml;
+
+    // Select all <h1> elements and extract text and ID
+    const h1Elements = tempDiv.querySelectorAll("h1");
+    const tocItems = Array.from(h1Elements).map((heading, index) => {
+      // Generate an ID based on the heading text or index
+      let headingId = heading.innerText
+        .toLowerCase()
+        .replace(/\s+/g, "-")
+        .replace(/[^\w-]/g, "");
+
+      // Ensure the ID is unique
+      if (tempDiv.querySelectorAll(`#${headingId}`).length > 0) {
+        headingId = `${headingId}-${index}`;
+      }
+
+      heading.id = headingId;
+      return { text: heading.innerText, id: heading.id };
+    });
+
+    // Update the state
+    setHeadings(tocItems);
+    setContentHtml(tempDiv.innerHTML);
+  }, [postData.contentHtml]);
+
   return (
     <div>
       <Header />
@@ -13,7 +53,21 @@ export default function Writeup({ postData }) {
         <AuthorDate author={postData.author} authorImage={postData.authorImage} date={postData.date} />
       </TitlePage>
       <TextWrap>
-        <div className="innerHTML" dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        <h2>Excerpt</h2>
+        <p>{postData.exerpt}</p>
+        <nav>
+          <h2>Table of Contents</h2>
+          <ul>
+            {headings.map((heading) => (
+              <li key={heading.id}>
+                <a href={`#${heading.id}`}>{heading.text}</a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </TextWrap>
+      <TextWrap>
+        <div className="innerHTML" dangerouslySetInnerHTML={{ __html: contentHtml }} />
       </TextWrap>
       <Footer />
     </div>
