c648046219af8f74fbed4b2a3df247c4f5ec8ead
Meyers <augustmeyers14@gmail.com>
2024-11-13 11:36:56 -0500

    Format and add margin

--- pages/components/RecentPosts/index.js
+++ pages/components/RecentPosts/index.js
@@ -1,37 +1,27 @@
-import PostItem from "../RecentPostItem"
-import { formatRelative } from 'date-fns'
+import PostItem from "../RecentPostItem";
+import { formatRelative } from "date-fns";
 
 export default function RecentPosts({ dir, allPostsData }) {
-    /*
-     * Basic checks before processing
-     *  - dir should not be invalid
-     */
-    if (allPostsData == null) {
-        console.error("allPostsData cannot be null")
-        return
+  /*
+   * Basic checks before processing
+   *  - dir should not be invalid
+   */
+  if (allPostsData == null) {
+    console.error("allPostsData cannot be null");
+    return;
+  }
 
-    }
+  function getDate(date) {
+    const raw = formatRelative(date, new Date(), { addSuffix: true });
 
-    function getDate(date) {
-      const raw = formatRelative(date, new Date(), { addSuffix: true })
+    return raw.charAt(0).toUpperCase() + raw.slice(1);
+  }
 
-      return raw.charAt(0).toUpperCase() + raw.slice(1) 
-
-    }
-
-    return (
-        <div>
-        {allPostsData.map(({ id, date, title, exerpt, image }) => (
-          <PostItem 
-            dir={dir}
-            key={id} 
-            id={id} 
-            title={title} 
-            date={getDate(date)} 
-            exerpt={exerpt} 
-            image={image} 
-          />
-        ))}
-      </div>
-    );
+  return (
+    <div style={{marginTop: "3rem", marginBottom: "3rem"}}>
+      {allPostsData.map(({ id, date, title, exerpt, image }) => (
+        <PostItem dir={dir} key={id} id={id} title={title} date={getDate(date)} exerpt={exerpt} image={image} />
+      ))}
+    </div>
+  );
 }
