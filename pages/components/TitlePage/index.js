462899e6da7e45d1cbbb1632f4bdef9fed16e2fd
Meyers <augustmeyers14@gmail.com>
2024-11-26 14:45:21 -0500

    Sidequest, made title page more modular

--- pages/components/TitlePage/index.js
+++ pages/components/TitlePage/index.js
@@ -1,26 +1,18 @@
 import styles from "./titlepage.module.css";
-import SocialLinks from "../SocialLinks"
+import SocialLinks from "../SocialLinks";
 import Image from "next/image";
-import Shadow from "../../../styles/Shadow.module.css"
+import Shadow from "../../../styles/Shadow.module.css";
 
-export default function TitlePage({ header, image, children }) {
-  if (header == null) {
-    console.error("Header for title cannot be null")
-    return 
-
-  }
-
+export default function TitlePage({ header, image, children, socialLinks = true }) {
   if (image == null) {
-    image = "/images/components/TitlePage/default.svg"
-
+    image = "/images/components/TitlePage/default.svg";
   }
 
   return (
     <div className={styles.wrapper}>
-      <h1 className={[styles.wrh1, Shadow.class].join(" ")}>{header}</h1>
-      <Image src={image} alt="Background image" height="500" width="500" priority={true}  />
-      <SocialLinks />
-
+      {header && <h1 className={[styles.wrh1, Shadow.class].join(" ")}>{header}</h1>}
+      <Image src={image} alt="Background image" height="500" width="500" priority={true} />
+      {socialLinks && <SocialLinks />}
       {children}
     </div>
   );
