611e47ebdc9e4ca91f8a726644a56820c3c7df70
meyersa <88554476+meyersa@users.noreply.github.com>
2025-01-22 23:07:01 -0500

    More simplistic styling on smallbox

--- pages/components/SmallBox/index.js
+++ pages/components/SmallBox/index.js
@@ -1,13 +1,17 @@
 import styles from "./smallbox.module.css";
 import SpacerDots from "../SpacerDots";
+import TextWrap from "../TextWrap";
 
 export default function SmallBox({ children }) {
-
   return (
-      <div className={styles.wrapper}>
+    <TextWrap>
+
+    <div className={styles.wrapper}>
         <SpacerDots />
         {children}
         <SpacerDots />
-      </div>
+    </div>
+    </TextWrap>
+
   );
 }
