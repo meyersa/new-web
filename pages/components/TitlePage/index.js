d786c2fd531edf398e4e799c85b344273cb0b437
Meyers <augustmeyers14@gmail.com>
2025-02-03 13:35:34 -0500

    Try to optimize cover images

--- pages/components/TitlePage/index.js
+++ pages/components/TitlePage/index.js
@@ -1,16 +1,23 @@
 import styles from "./titlepage.module.css";
 import SocialLinks from "../SocialLinks";
 import Image from "next/image";
+import defaultImage from "../../../public/images/components/TitlePage/default.svg";
 
 export default function TitlePage({ header, image, children, socialLinks = true }) {
   if (image == null) {
-    image = "/images/components/TitlePage/default.svg";
+    image = defaultImage;
   }
 
   return (
     <div className={styles.wrapper}>
       {header && <h1 className={styles.wrh1}>{header}</h1>}
-      <Image src={image} alt="Background image" height="500" width="500" priority={true} />
+      {typeof image === "string" ? (
+        // Dynamic image
+        <Image src={image} alt="Background image" priority quality={50} width={500} height={500} />
+      ) : (
+        // Static imported image
+        <Image src={image || defaultImage} alt="Background image" priority quality={50} placeholder="blur" />
+      )}{" "}
       {socialLinks && <SocialLinks />}
       {children}
     </div>
