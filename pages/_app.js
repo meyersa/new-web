0460362ffc4d2633f19b0b543b044133887a2ac3
Meyers <augustmeyers14@gmail.com>
2025-02-03 15:02:25 -0500

    Switch to Cloduflare CDNJS

--- pages/_app.js
+++ pages/_app.js
@@ -34,7 +34,7 @@ export default function App({ Component, pageProps }) {
       </Head>
 
       {/* Load jQuery */}
-      <Script src="https://code.jquery.com/jquery-3.7.1.min.js" />
+      <Script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" />
 
       {/* Helper Component */}
       <ScrollHelper />
