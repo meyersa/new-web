9c7d0711c16748ffd0c3e4fd582eb804b213c78f
Meyers <augustmeyers14@gmail.com>
2025-02-03 17:13:24 -0500

    Back to constant size for more consistency

--- pages/components/TitlePage/index.js
+++ pages/components/TitlePage/index.js
@@ -11,13 +11,7 @@ export default function TitlePage({ header, image, children, socialLinks = true 
   return (
     <div className={styles.wrapper}>
       {header && <h1 className={styles.wrh1}>{header}</h1>}
-      {typeof image === "string" ? (
-        // Dynamic image
-        <Image src={image} alt="Background image" priority={true} quality={25} fill={true} />
-      ) : (
-        // Static imported image
-        <Image src={image || defaultImage} alt="Background image" priority={true} quality={25} fill={true} placeholder="blur" />
-      )}{" "}
+      <Image src={image || defaultImage} alt="Background image" priority={true} quality={25} height={"500"} width={"500"} placeholder="blur" />
       {socialLinks && <SocialLinks />}
       {children}
     </div>
