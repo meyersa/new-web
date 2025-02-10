5077f76f71b682a3ba862cbac07c70d350013142
Meyers <augustmeyers14@gmail.com>
2025-02-10 17:44:54 -0500

    Remove second dot for simplicity

--- pages/components/SmallBox/index.js
+++ pages/components/SmallBox/index.js
@@ -1,15 +1,11 @@
 import styles from "./smallbox.module.css";
 import SpacerDots from "../SpacerDots";
-import TextWrap from "../TextWrap";
 
 export default function SmallBox({ children }) {
   return (
-    <TextWrap>
-      <div className={styles.wrapper}>
-        <SpacerDots />
-        {children}
-        <SpacerDots />
-      </div>
-    </TextWrap>
+    <div className={styles.wrapper}>
+      <SpacerDots />
+      {children}
+    </div>
   );
 }
