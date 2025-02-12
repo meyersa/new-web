b7e02d564d8d4770bd1484c7de5d5702ffe3d375
Meyers <augustmeyers14@gmail.com>
2025-02-11 23:38:10 -0500

    OG:Image tag based on title image

--- pages/components/TitlePage/index.js
+++ pages/components/TitlePage/index.js
@@ -1,8 +1,9 @@
 import styles from "./titlepage.module.css";
 import Image from "next/image";
-import defaultImage from "../../../public/images/components/TitlePage/default.svg"
-import Width from "../../../styles/Width.module.css"
-import SocialLinks from "../SocialLinks"
+import defaultImage from "../../../public/images/components/TitlePage/default.svg";
+import Width from "../../../styles/Width.module.css";
+import SocialLinks from "../SocialLinks";
+import Head from "next/head";
 
 export default function TitlePage({ header, image, children }) {
   if (image == null) {
@@ -10,17 +11,30 @@ export default function TitlePage({ header, image, children }) {
   }
 
   return (
-    <div className={`${styles.wrapper} ${Width.default}`}>
-      {header && <h1 className={styles.wrh1}>{header}</h1>}
-      {typeof image === "string" ? (
-        // Dynamic image
-        <Image src={image} alt="Background image" priority={true} quality={25} height={"500"} width={"500"} />
-      ) : (
-        // Static imported image
-        <Image src={image || defaultImage} alt="Background image" priority={true} quality={25} height={"500"} width={"500"} placeholder="blur" />
-      )}{" "}
-      {children}
-      <SocialLinks />
-    </div>
+    <>
+      <Head>
+        <meta property="og:image" content={image} />
+      </Head>
+      <div className={`${styles.wrapper} ${Width.default}`}>
+        {header && <h1 className={styles.wrh1}>{header}</h1>}
+        {typeof image === "string" ? (
+          // Dynamic image
+          <Image src={image} alt="Background image" priority={true} quality={25} height={"500"} width={"500"} />
+        ) : (
+          // Static imported image
+          <Image
+            src={image || defaultImage}
+            alt="Background image"
+            priority={true}
+            quality={25}
+            height={"500"}
+            width={"500"}
+            placeholder="blur"
+          />
+        )}{" "}
+        {children}
+        <SocialLinks />
+      </div>
+    </>
   );
 }
