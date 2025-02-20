60c7b588f45b998c3cb7ee5f1d9dc6dc44001df1
meyersa <88554476+meyersa@users.noreply.github.com>
2025-02-20 10:34:07 -0500

    Add back color to icons

--- pages/photography.js
+++ pages/photography.js
@@ -8,7 +8,12 @@ import TextBlock from "./components/TextBlock";
 import PageBlock from "./components/PageBlock";
 import ShowcaseItem from "./components/ShowcaseItem";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCamera, faCameraRetro, faMagnifyingGlass, faSignOut } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCamera,
+  faCameraRetro,
+  faMagnifyingGlass,
+  faSignOut,
+} from "@fortawesome/free-solid-svg-icons";
 import FASection from "./components/FASection";
 import SmallBox from "./components/SmallBox";
 import { getBlurData } from "../lib/getBlurData";
@@ -32,61 +37,69 @@ export default function Photography({ allPostsData, blurDataURL }) {
           <div>
             <h2>Trips, Portraits, Sports, and more</h2>
             <p>
-              On this page are my favorite collections of photos, mostly of trips I&apos;ve been on, but also including
-              some sports photography.
+              On this page are my favorite collections of photos, mostly of trips I&apos;ve been on,
+              but also including some sports photography.
             </p>
           </div>
           <ShowcaseItem>
             <h3>Equipment</h3>
             <FASection>
-              <FontAwesomeIcon icon={faCamera} />
+              <FontAwesomeIcon icon={faCamera} style={{ color: "var(--contrast)" }} />
               <div>
                 <h4>Canon EOS R (Primary)</h4>
                 <p>
-                  Canon&apos;s original full frame mirrorless that started the R lineup, it never disappoints. I picked
-                  this up off the Canon refurb store and have been shooting with it as my primary since
+                  Canon&apos;s original full frame mirrorless that started the R lineup, it never
+                  disappoints. I picked this up off the Canon refurb store and have been shooting
+                  with it as my primary since
                 </p>
               </div>
             </FASection>
             <FASection>
-              <FontAwesomeIcon icon={faCameraRetro} />
+              <FontAwesomeIcon icon={faCameraRetro} style={{ color: "var(--contrast)" }} />
               <div>
                 <h4>Canon 6D (Backup)</h4>
                 <p>
-                  While not as advanced as the primary R, the 6D serves as a reliable backup with better battery life
-                  and solid performance. However it generally serves as a convenient lens storage system for when I need
-                  to have multiple without a backpack
+                  While not as advanced as the primary R, the 6D serves as a reliable backup with
+                  better battery life and solid performance. However it generally serves as a
+                  convenient lens storage system for when I need to have multiple without a backpack
                 </p>
               </div>
             </FASection>
             <FASection>
-              <FontAwesomeIcon icon={faMagnifyingGlass} />
+              <FontAwesomeIcon icon={faMagnifyingGlass} style={{ color: "var(--contrast)" }} />
               <div>
                 <h4>Tamron 100-400</h4>
                 <p>
-                  My trusted zoom lens for sports and wildlife photography. Occasionally this can turn into a portrait
-                  lens or landscape lens in a pinch, but the minimum zoom makes it a little tricky
+                  My trusted zoom lens for sports and wildlife photography. Occasionally this can
+                  turn into a portrait lens or landscape lens in a pinch, but the minimum zoom makes
+                  it a little tricky
                 </p>
               </div>
             </FASection>
             <FASection>
-              <FontAwesomeIcon icon={faSignOut} />
+              <FontAwesomeIcon icon={faSignOut} style={{ color: "var(--contrast)" }} />
               <div>
                 <h4>Sigma 24-105 Art</h4>
                 <p>
-                  The companion to my zoom, for everything else, this lens performs pretty well. It&apos;s not quite as
-                  sharp as I would hope though, and in the future this would probably be the first thing to get upgraded
+                  The companion to my zoom, for everything else, this lens performs pretty well.
+                  It&apos;s not quite as sharp as I would hope though, and in the future this would
+                  probably be the first thing to get upgraded
                 </p>
               </div>
             </FASection>
           </ShowcaseItem>
           <SmallBox>
-            <p>As for software, everything is done on Lightroom Classic as I&apos;m pretty simple</p>
+            <p>
+              As for software, everything is done on Lightroom Classic as I&apos;m pretty simple
+            </p>
           </SmallBox>
         </PageBlock>
         <PageBlock>
           <h2>Recent Posts</h2>
           <RecentPosts dir={type} allPostsData={allPostsData} postsPerPage={5} />
+          <SmallBox>
+            <p>Not featured here are a lot of the small projects I&apos;ve worked on such as photographing for clubs, friends, and family.</p>
+          </SmallBox>
         </PageBlock>
       </TextBlock>
       <Footer />
