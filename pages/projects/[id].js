8cc01833a5685fc00ad0f7f7b0e524e2f755a287
meyersa <88554476+meyersa@users.noreply.github.com>
2025-02-20 10:34:18 -0500

    Use context

--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -6,13 +6,14 @@ import TextWrap from "../components/TextWrap";
 import "@wooorm/starry-night/style/dark";
 import Head from "next/head";
 import TextBlock from "../components/TextBlock";
-import ShowcaseItem from "../components/ShowcaseItem";
 import FASection from "../components/FASection";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithubAlt } from "@fortawesome/free-brands-svg-icons";
-import { faLink } from "@fortawesome/free-solid-svg-icons";
+import { faCalendar, faComment, faExternalLink } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
 import { getBlurData } from "../../lib/getBlurData";
+import Context from "../components/Context";
+import PageBlock from "../components/PageBlock";
 
 const type = "projects";
 
@@ -25,34 +26,62 @@ export default function Projects({ postData, blurDataURL }) {
       </Head>
       <Header />
       <TitlePage header={postData.title} image={postData.image} blurDataURL={blurDataURL}>
-        <p>{postData.excerpt}</p>
-        <p>{postData.date}</p>
+        <p>
+          <FontAwesomeIcon
+            icon={faCalendar}
+            className="fa-solid"
+            style={{ fontSize: "var(--small-font)", color: "var(--contrast)" }}
+          />{" "}
+          {postData.excerpt}
+        </p>
+        <p>
+          <FontAwesomeIcon
+            icon={faComment}
+            className="fa-solid"
+            style={{ fontSize: "var(--small-font)", color: "var(--contrast)" }}
+          />{" "}
+          {postData.date}
+        </p>
       </TitlePage>
       <TextBlock>
-        {postData.github || postData.demo ? (
-          <ShowcaseItem>
-            <h3>Links</h3>
-            {postData.github ? (
-              <FASection>
-                <FontAwesomeIcon icon={faGithubAlt} />
-                <p>
-                  Check out the source code on <Link href={postData.github}>Github</Link>
-                </p>
-              </FASection>
-            ) : null}
-            {postData.demo ? (
-              <FASection>
-                <FontAwesomeIcon icon={faLink} />
-                <p>
-                  Check out the <Link href={postData.demo}>Demo</Link>
-                </p>
-              </FASection>
-            ) : null}
-          </ShowcaseItem>
-        ) : null}
-        <TextWrap>
-          <div className="innerHTML" dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-        </TextWrap>
+        <PageBlock>
+          {postData.github || postData.demo ? (
+            <Context>
+              <h3>Links</h3>
+              {postData.github ? (
+                <FASection>
+                  <Link href={postData.github}>
+                    <FontAwesomeIcon
+                      icon={faGithubAlt}
+                      className="fa-solid fa-fw"
+                      style={{ color: "var(--contrast)" }}
+                    />
+                  </Link>
+                  <p>
+                    Check out the source code on <Link href={postData.github}>Github</Link>
+                  </p>
+                </FASection>
+              ) : null}
+              {postData.demo ? (
+                <FASection>
+                  <Link href={postData.demo}>
+                    <FontAwesomeIcon
+                      icon={faExternalLink}
+                      className="fa-solid fa-fw"
+                      style={{ color: "var(--contrast)" }}
+                    />
+                  </Link>
+                  <p>
+                    Check out the <Link href={postData.demo}>Demo</Link>
+                  </p>
+                </FASection>
+              ) : null}
+            </Context>
+          ) : null}
+          <TextWrap>
+            <div className="innerHTML" dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+          </TextWrap>
+        </PageBlock>
       </TextBlock>
       <Footer />
     </>
@@ -67,12 +96,12 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData =  await getPostData(params.id); 
+  const postData = await getPostData(params.id);
 
   return {
     props: {
       postData: postData,
-      blurDataURL: await getBlurData(postData.image)
+      blurDataURL: await getBlurData(postData.image),
     },
   };
 }
