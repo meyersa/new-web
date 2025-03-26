005d024e34593ad63750e667a972129fa174076a
Meyers <augustmeyers14@gmail.com>
2025-03-26 10:48:45 -0400

    Match other components

--- pages/components/Context/index.js
+++ pages/components/Context/index.js
@@ -1,11 +1,11 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./context.module.css";
-import { faComment } from "@fortawesome/free-solid-svg-icons";
+import { faComment } from "@fortawesome/free-regular-svg-icons";
 
 export default function Context({ children }) {
   return (
     <div className={styles.wrapper}>
-      <FontAwesomeIcon icon={faComment} className="fa-solid" />
+      <FontAwesomeIcon icon={faComment} className="fa-regular fa-flip-horizontal" />
       <div className={styles.divider} />
       <div className={styles.wrapperInner}>{children}</div>
     </div>
