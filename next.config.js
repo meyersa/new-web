12333980c5055b5235c7a22c38bcdd2dde2ea34e
meyersa <88554476+meyersa@users.noreply.github.com>
2025-03-28 14:31:53 -0400

    Change loading of images in config instead

--- next.config.js
+++ next.config.js
@@ -1,35 +1,38 @@
-const isDev = process.env.NODE_ENV === 'development';
+const isDev = process.env.NODE_ENV === "development";
 
 module.exports = {
   output: "standalone",
-  images: {
-    loader: 'custom',
-    loaderFile: './lib/cfLoader.js',
-    remotePatterns: isDev ? 
-    [
-      {
-        hostname: 'github.com',
+  images: isDev
+    ? {
+        remotePatterns: [
+          {
+            hostname: "github.com",
+          },
+          {
+            hostname: "localhost",
+          },
+        ],
+      }
+    : {
+        loader: "custom",
+        loaderFile: "./lib/cfLoader.js",
+        remotePatterns: [
+          {
+            hostname: "github.com",
+          },
+          {
+            hostname: "cdn.meyersa.com",
+          },
+        ],
       },
-      {
-        hostname: 'localhost',
-      },
-    ] : [
-      {
-        hostname: 'github.com',
-      },
-      {
-        hostname: 'cdn.meyersa.com',
-      },
-    ]
-  },
   async redirects() {
     return [
       {
-        source: '/sitemap.xml',
-        destination: '/api/sitemap',
-        permanent: true
-      }
-    ]
+        source: "/sitemap.xml",
+        destination: "/api/sitemap",
+        permanent: true,
+      },
+    ];
   },
   i18n: {
     locales: ["en"],
